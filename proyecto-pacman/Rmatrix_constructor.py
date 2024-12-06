125f026e7d26f10d4366236c3d3e1d454ada3663
AndyMine12 <andresale.fq@gmail.com>
2024-12-06 14:26:41 -0400

    Map loading
    Added script to load the map structure from a txt file, and translate it to a base r-matrix to populate with heat maps

---
+++ proyecto-pacman/Rmatrix_constructor.py
@@ -0,0 +1,50 @@
+IN_FILENAME = "map.txt"
+OUT_FILENAME = "base_r.txt"
+base_map: list[int] = []
+wall_index: list[int] = []
+
+#LOAD MAP
+with open(IN_FILENAME, 'r') as file:
+    map_height:int = 0
+    map_width:int = -1
+    for row,line in enumerate(file.readlines()):
+        map_height += 1
+        line = line.strip("\n")
+
+        if (map_width == -1):
+            map_width = len(line)
+        elif (map_width != len(line)):
+            raise ValueError("Map must be of fixed width")
+        
+        for i in range(map_width):
+            base_map.append(0)
+
+        for column,char in enumerate(line):
+            if (char == "O"):
+                base_map[map_width*row + (column)] = 0 #Set current square as 'legal' move
+            elif (char == "X"):
+                base_map[map_width*row + (column)] = -1 #Set current square as 'illegal' move
+                wall_index.append(map_width*row + (column))
+            else:
+                raise ValueError("Squares must be either spaces (O) or walls (X)")
+    
+    print(f"SIZE {map_width}x{map_height}")
+    acc = 0
+    for square in base_map:
+        if square == 0:
+            print("O", end="")
+        else:
+            print("\033[91m" + "X" + "\033[0m", end="")
+        acc += 1
+        if (acc >= map_width):
+            acc = 0
+            print()
+    print(f"\nWALLS ({len(wall_index)})\n",wall_index)
+
+#SAVE BASE R-MATRIX
+with open(OUT_FILENAME, 'w') as file:
+    for index,value in enumerate(base_map):
+        file.write(str(value))
+        if index < (len(base_map) - 1):
+            file.write("|")
+    print(f"\nSaved result to file at ({OUT_FILENAME})")
